5afce3aab678ace4eb4a9b3d05658b2ed5ce7163
Edvard Kvisler Ladim <eddy.ladim@gmail.com>
2026-02-04 15:19:11 +0100

    feat: start developing Player and Transaction classes for managing transactions

---
+++ src/main/java/edu/ntni/idi/idatt/millions/transaction/Purchase.java
@@ -0,0 +1,15 @@
+package edu.ntni.idi.idatt.millions.transaction;
+
+import edu.ntni.idi.idatt.millions.Player;
+import edu.ntni.idi.idatt.millions.PurchaseCalculator;
+import edu.ntni.idi.idatt.millions.Share;
+
+public class Purchase extends Transaction{
+  public Purchase(Share share, int week) {
+    super(share, week, new PurchaseCalculator());
+  }
+  @Override
+  public void commit(Player player) {
+
+  }
+}
---
+++ src/main/java/edu/ntni/idi/idatt/millions/transaction/Transaction.java
@@ -0,0 +1,38 @@
+package edu.ntni.idi.idatt.millions.transaction;
+
+import edu.ntni.idi.idatt.millions.Player;
+import edu.ntni.idi.idatt.millions.Share;
+import edu.ntni.idi.idatt.millions.TransactionCalculator;
+
+public abstract class Transaction {
+  private Share share;
+  private int week;
+  private TransactionCalculator calculator;
+  private boolean committed;
+
+  protected Transaction(Share share, int week, TransactionCalculator calculator) {
+    this.share = share;
+    this.week = week;
+    this.calculator = calculator;
+  }
+
+  public Share getShare() {
+    return share;
+  }
+
+  public int getWeek() {
+    return week;
+  }
+
+  public edu.ntni.idi.idatt.millions.TransactionCalculator getCalculator() {
+    return calculator;
+  }
+
+  public boolean isCommitted() {
+    return committed;
+  }
+
+  public void commit(Player player) {
+
+  }
+}

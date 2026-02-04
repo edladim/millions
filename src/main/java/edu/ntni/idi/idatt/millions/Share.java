f33e90ab5bbbba3ea6f0bfee9679e02abf3291f5
Edvard Kvisler Ladim <eddy.ladim@gmail.com>
2026-02-04 12:38:53 +0100

    feat: add Share class and update .gitignore

---
+++ src/main/java/edu/ntni/idi/idatt/millions/Share.java
@@ -0,0 +1,27 @@
+package edu.ntni.idi.idatt.millions;
+
+import java.math.BigDecimal;
+
+public class Share {
+  private Stock stock;
+  private BigDecimal quantity;
+  private BigDecimal purchasePrice;
+
+  public Share(Stock stock, BigDecimal quantity, BigDecimal purchasePrice) {
+    this.stock = stock;
+    this.quantity = quantity;
+    this.purchasePrice = purchasePrice;
+  }
+
+  public Stock getStock() {
+    return stock;
+  }
+
+  public BigDecimal getQuantity() {
+    return quantity;
+  }
+
+  public BigDecimal getPurchasePrice() {
+    return purchasePrice;
+  }
+}

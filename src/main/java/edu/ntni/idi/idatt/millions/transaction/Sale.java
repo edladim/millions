56668d787cd9a7c332a783ab159b6fe2234d4804
hellumreppen <oskar.hellum.reppen@gmail.com>
2026-02-18 16:28:01 +0100

    feat: enhance Purchase class with transaction validation, then commit transaction

--- src/main/java/edu/ntni/idi/idatt/millions/transaction/Sale.java
+++ src/main/java/edu/ntni/idi/idatt/millions/transaction/Sale.java
@@ -2,11 +2,12 @@ package edu.ntni.idi.idatt.millions.transaction;
 
 import edu.ntni.idi.idatt.millions.Player;
 import edu.ntni.idi.idatt.millions.Share;
+import edu.ntni.idi.idatt.millions.calculator.SaleCalculator;
 
 public class Sale extends Transaction {
 
   public Sale(Share share, int week) {
-    super(share, week, new SaleCalculator());
+    super(share, week, new SaleCalculator(share));
   }
 
   @Override
--- src/main/java/edu/ntni/idi/idatt/millions/transaction/Transaction.java
+++ src/main/java/edu/ntni/idi/idatt/millions/transaction/Transaction.java
@@ -28,6 +28,10 @@ public abstract class Transaction {
     return calculator;
   }
 
+  public void setCommitted(boolean committed) {
+    this.committed = committed;
+  }
+
   public boolean isCommitted() {
     return committed;
   }

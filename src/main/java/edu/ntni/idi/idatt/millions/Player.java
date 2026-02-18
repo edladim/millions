7b5d0aaeb7201f1ac42dfdf6ecb910166a37d432
hellumreppen <oskar.hellum.reppen@gmail.com>
2026-02-18 15:56:06 +0100

    fix: correct money handling in Player class constructor and methods

--- src/main/java/edu/ntni/idi/idatt/millions/Player.java
+++ src/main/java/edu/ntni/idi/idatt/millions/Player.java
@@ -12,6 +12,7 @@ public class Player {
   public Player(String name, BigDecimal stratingMoney) {
     this.name = name;
     this.stratingMoney = stratingMoney;
+    this.money = stratingMoney;
     this.portfolio = new Portfolio();
     this.transactionArchive = new TransactionArchive();
   }
@@ -24,12 +25,12 @@ public class Player {
     return money;
   }
 
-  public void addMoney(BigDecimal money) {
-    this.money = this.money.add(money);
+  public void addMoney(BigDecimal amount) {
+    money = money.add(amount);
   }
 
   public void withdrawMoney(BigDecimal amount) {
-
+    money = money.subtract(amount);
   }
 
 

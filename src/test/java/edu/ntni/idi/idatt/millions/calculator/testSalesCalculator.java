39c6a2b344bc53dc584755d2e9329a667c5d48a1
hellumreppen <oskar.hellum.reppen@gmail.com>
2026-03-04 10:18:33 +0100

    test: add setup method for SalesCalculator tests

--- src/test/java/edu/ntni/idi/idatt/millions/calculator/testSalesCalculator.java
+++ src/test/java/edu/ntni/idi/idatt/millions/calculator/testSalesCalculator.java
@@ -1,5 +1,21 @@
 package edu.ntni.idi.idatt.millions.calculator;
 
+import edu.ntni.idi.idatt.millions.Share;
+import edu.ntni.idi.idatt.millions.Stock;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.math.BigDecimal;
+
 public class testSalesCalculator {
+
+  private SaleCalculator saleCalculator;
+  private Share share;
+
+  @BeforeEach
+  void setUp() {
+    Stock stock = new Stock("AAPL", "Apple Inc.", BigDecimal.valueOf(200));
+    share = new Share(stock, BigDecimal.valueOf(100), BigDecimal.valueOf(10));
+    saleCalculator = new SaleCalculator(share);
+  }
 
 }

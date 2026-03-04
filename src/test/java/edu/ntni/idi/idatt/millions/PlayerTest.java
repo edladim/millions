4d90da3b03b3bafd956f19d09c7335f7d6321ea0
hellumreppen <oskar.hellum.reppen@gmail.com>
2026-03-04 11:22:47 +0100

    test: add unit tests for Player class initialization

--- src/test/java/edu/ntni/idi/idatt/millions/PlayerTest.java
+++ src/test/java/edu/ntni/idi/idatt/millions/PlayerTest.java
@@ -1,4 +1,20 @@
 package edu.ntni.idi.idatt.millions;
 
+import edu.ntni.idi.idatt.millions.model.Player;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.math.BigDecimal;
+
 public class PlayerTest {
+
+  private Player player;
+  private BigDecimal startingMoney;
+
+  @BeforeEach
+  void setup() {
+    startingMoney = BigDecimal.valueOf(10000);
+    player = new Player("Test Player", startingMoney);
+  }
+
+
 }

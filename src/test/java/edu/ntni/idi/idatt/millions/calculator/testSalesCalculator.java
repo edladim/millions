165ec2af4da1d4df786822ff0e88dbd620314ea1
hellumreppen <oskar.hellum.reppen@gmail.com>
2026-03-04 10:24:03 +0100

    test: add unit tests for SaleCalculator methods

--- src/test/java/edu/ntni/idi/idatt/millions/calculator/testSalesCalculator.java
+++ src/test/java/edu/ntni/idi/idatt/millions/calculator/testSalesCalculator.java
@@ -3,8 +3,11 @@ package edu.ntni.idi.idatt.millions.calculator;
 import edu.ntni.idi.idatt.millions.Share;
 import edu.ntni.idi.idatt.millions.Stock;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class testSalesCalculator {
 
@@ -17,5 +20,18 @@ public class testSalesCalculator {
     share = new Share(stock, BigDecimal.valueOf(100), BigDecimal.valueOf(10));
     saleCalculator = new SaleCalculator(share);
   }
+
+  @Test
+  void testCalculateGross() {
+    BigDecimal expected = BigDecimal.valueOf(2000);
+    assertEquals(expected, saleCalculator.calculateGross());
+  }
+
+  @Test
+  void testCalculateCommission() {
+    BigDecimal expected = BigDecimal.valueOf(20);
+    assertEquals(expected, saleCalculator.calculateComission());
+  }
+
 
 }

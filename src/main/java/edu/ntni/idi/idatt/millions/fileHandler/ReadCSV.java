e3294335cdfa9c82793f32173e67a1539e87d20b
hellumreppen <oskar.hellum.reppen@gmail.com>
2026-03-09 15:52:06 +0100

    fix: readStockData now returns list of all stocks from StockData.csv. Also ignores lines starting with "#" and blank lines.

--- src/main/java/edu/ntni/idi/idatt/millions/fileHandler/ReadCSV.java
+++ src/main/java/edu/ntni/idi/idatt/millions/fileHandler/ReadCSV.java
@@ -1,24 +1,39 @@
 package edu.ntni.idi.idatt.millions.fileHandler;
 
+import edu.ntni.idi.idatt.millions.model.Stock;
+
 import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReadCSV {
+  private String stockDataFilePath = "src/main/resources/StockData.csv";
 
-  public static void readFile(String filePath) {
-    try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))) {
+  public List<Stock> readStockData() {
+    List<Stock> stocks = new ArrayList<>();
+
+    try (InputStream is = getClass().getResourceAsStream("/StockData.csv");
+          BufferedReader bf = new BufferedReader(new InputStreamReader(is))) {
+
       String line;
-      String delimiter = ",";
-      while ((line = bufferedReader.readLine()) != null) {
-        String[] fields = line.split(delimiter);
-        for (String field : fields) {
+      while ((line = bf.readLine()) != null) {
+        if (line.startsWith("#") || line.isBlank()) continue;
 
+        String[] fields = line.split(",");
+        if (fields.length == 3) {
+          String symbol = fields[0].trim();
+          String company = fields[1].trim();
+          BigDecimal salesPrice = new BigDecimal(fields[2].trim());
+          stocks.add(new Stock(symbol, company, salesPrice));
         }
       }
     } catch (Exception e) {
       e.printStackTrace();
     }
+    return stocks;
   }
 
 }

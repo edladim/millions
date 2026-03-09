822e6ad2d8c442bce089eeb776df6bfc4c30f54e
hellumreppen <oskar.hellum.reppen@gmail.com>
2026-03-09 15:29:11 +0100

    feat: implement readFile method in ReadCSV class for reading CSV files

--- src/main/java/edu/ntni/idi/idatt/millions/fileHandler/ReadCSV.java
+++ src/main/java/edu/ntni/idi/idatt/millions/fileHandler/ReadCSV.java
@@ -1,4 +1,24 @@
 package edu.ntni.idi.idatt.millions.fileHandler;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.math.BigDecimal;
+
 public class ReadCSV {
+
+  public static void readFile(String filePath) {
+    try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))) {
+      String line;
+      String delimiter = ",";
+      while ((line = bufferedReader.readLine()) != null) {
+        String[] fields = line.split(delimiter);
+        for (String field : fields) {
+
+        }
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
 }

0ad34feb2973e1c4fa54531b743b50257e0376b7
Oskar Hellum-Reppen <90929819+hellumreppen@users.noreply.github.com>
2026-05-09 13:18:31 +0200

    Remove unused import statement
    Removed unused import statement for Objects.

--- src/main/java/edu/ntnu/idi/idatt/millions/view/MainView.java
+++ src/main/java/edu/ntnu/idi/idatt/millions/view/MainView.java
@@ -4,7 +4,6 @@ import edu.ntnu.idi.idatt.millions.view.components.SidebarComponent;
 import edu.ntnu.idi.idatt.millions.view.pages.DashboardView;
 import edu.ntnu.idi.idatt.millions.view.pages.PortfolioView;
 import edu.ntnu.idi.idatt.millions.view.pages.TradingView;
-import java.util.Objects;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 

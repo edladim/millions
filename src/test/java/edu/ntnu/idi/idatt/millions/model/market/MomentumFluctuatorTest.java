9cd0e0e926492675eaf652c6976bda1c0ff27063
hellumreppen <oskar.hellum.reppen@gmail.com>
2026-05-25 14:45:34 +0200

    feat: add unit tests for MomentumFluctuator to validate market event handling

---
+++ src/test/java/edu/ntnu/idi/idatt/millions/model/market/MomentumFluctuatorTest.java
@@ -0,0 +1,47 @@
+package edu.ntnu.idi.idatt.millions.model.market;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MomentumFluctuatorTest {
+
+  private MomentumFluctuator fluctuator;
+
+  @BeforeEach
+  void setUp() {
+    fluctuator = new MomentumFluctuator();
+  }
+
+  private static final class ConstantRandom extends Random {
+    private final double value;
+
+    private ConstantRandom(double value) {
+      this.value = value;
+    }
+
+    @Override
+    public double nextDouble() {
+      return value;
+    }
+  }
+
+  @Test
+  void beginWeek_handlesMarketEventAndNonEvent() {
+    fluctuator.beginWeek(new ConstantRandom(0.0));
+    BigDecimal priceAfterEvent = fluctuator.nextPrice(
+        "AAA", new BigDecimal("10"), new ConstantRandom(1.0));
+    assertTrue(priceAfterEvent.compareTo(BigDecimal.ONE) > 0);
+
+    fluctuator.beginWeek(new ConstantRandom(1.0));
+    BigDecimal priceAfterNoEvent = fluctuator.nextPrice(
+        "BBB", new BigDecimal("10"), new ConstantRandom(1.0));
+    assertTrue(priceAfterNoEvent.compareTo(BigDecimal.ONE) > 0);
+  }
+
+
+}

68a937bbe402b0249eef5d62b67b8d1cbe314f31
hellumreppen <oskar.hellum.reppen@gmail.com>
2026-05-26 19:06:42 +0200

    docs: update Javadoc for getHistoricalNetWorth method in ReadOnlyPlayer

--- src/main/java/edu/ntnu/idi/idatt/millions/model/player/ReadOnlyPlayer.java
+++ src/main/java/edu/ntnu/idi/idatt/millions/model/player/ReadOnlyPlayer.java
@@ -34,11 +34,10 @@ public interface ReadOnlyPlayer {
    */
   BigDecimal getNetWorth();
 
-
   /**
    * Returns the player's historical net worth values, ordered from oldest to most recent.
    *
-   * <p>The returned list is an unmodifiable snapshot and cannot be modified by callers.</p>
+   * <p>The returned list is an unmodifiable snapshot and cannot be modified by callers.
    *
    * @return an unmodifiable list of net worth values
    */
